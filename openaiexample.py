a7b3ee270b2114465d65d36280cbbd69f9ae16b1
Kevin Wang <kevinwang824@outlook.com>
2017-07-31 19:38:38 +0800

    Training_data
    Accumulate training data for neural network

--- openaiexample.py
+++ openaiexample.py
@@ -14,6 +14,7 @@ goal_step = 500
 score_requirement = 50
 initial_games = 10000
 
+
 def some_random_game_first():
     for episode in range(10):
         env.reset()
@@ -24,4 +25,50 @@ def some_random_game_first():
             if done:
                 break
 
-some_random_game_first()
+
+# some_random_game_first()
+
+
+def initial_population():
+    training_data = []
+    scores = []
+    accepted_scores = []
+    for _ in range(initial_games):
+        score = 0
+        game_memory = []
+        prev_observation = []
+        for _ in range(goal_step):
+            action = random.randrange(0, 2)
+            observation, reward, done, info = env.step(action)
+
+            if len(prev_observation) > 0:
+                game_memory.append([prev_observation, action])
+
+            prev_observation = observation
+            score += reward
+            if done:
+                break
+
+        if score >= score_requirement:
+            accepted_scores.append(score)
+            for data in game_memory:
+                if data[1] == 1:
+                    output = [0, 1]
+                elif data[1] == 0:
+                    output = [1, 0]
+
+                training_data.append([data[0], output])
+
+        env.reset()
+        scores.append(score)
+
+    training_data_save = np.array(training_data)
+    np.save('save.npy', training_data_save)
+
+    print('Average accepted score:', mean(accepted_scores))
+    print('Median accepted score:', median(accepted_scores))
+    print(Counter(accepted_scores))
+
+    return training_data
+
+initial_population()
