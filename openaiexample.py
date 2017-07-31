75364e5f834fb86eb73d5517816951bbf8c8bb4b
Kevin Wang <kevinwang824@outlook.com>
2017-07-31 20:02:03 +0800

    Training Model
    Training model and collecting data

--- openaiexample.py
+++ openaiexample.py
@@ -29,7 +29,7 @@ def some_random_game_first():
 # some_random_game_first()
 
 
-def initial_population():
+def initial_population() -> object:
     training_data = []
     scores = []
     accepted_scores = []
@@ -71,4 +71,44 @@ def initial_population():
 
     return training_data
 
-initial_population()
+
+def neural_network_model(input_size):
+    network = input_data(shape=[None, input_size, 1], name='input')
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+
+
+def train_model(training_data, model=False):
+    x = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1)
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size=len(x[0]))
+
+    model.fit({'input': x}, {'targets': y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openaistuff')
+
+    return model
+
+
+training_data = initial_population()
+model = train_model(training_data)
