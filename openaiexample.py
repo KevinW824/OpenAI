fe9f33a8fac8801113d8ee24de864e68bc9c15a1
Kevin Wang <kevinwang824@outlook.com>
2017-07-31 22:33:09 +0800

    Score!
    Done, and improve to get a higher score by changing the epoch etc.

--- openaiexample.py
+++ openaiexample.py
@@ -112,3 +112,31 @@ def train_model(training_data, model=False):
 
 training_data = initial_population()
 model = train_model(training_data)
+
+scores = []
+choices = []
+
+for each_game in range(10):
+    score = 0
+    game_memory = []
+    prev_obs = []
+    env.reset()
+    for _ in range(goal_step):
+        env.render()
+        if len(prev_obs) == 0:
+            action = random.randrange(0, 2)
+        else:
+            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1)) [0])
+        choices.append(action)
+
+        new_observation, reward, done, info = env.step(action)
+        prev_obs = new_observation
+        game_memory.append([new_observation, action])
+        score += reward
+        if done:
+            break
+    scores.append(score)
+
+print('Average Score', sum(scores) / len(scores))
+print('Choice 1: {}, Choice 0: {}'.format(choices.count(1) / len(choices),
+                                            choices.count(0) / len(choices)))
